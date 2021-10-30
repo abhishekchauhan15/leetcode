1fbc9735b1fca9e41384c2519daf4830f06912c7
Abhishek Chauhan <76480451+abhishekchauhan15@users.noreply.github.com>
2021-10-30 11:48:11 +0530

    Time: 8 ms (95.77%), Space: 8.1 MB (58.48%) - LeetHub

--- linked-list-cycle/linked-list-cycle.cpp
+++ linked-list-cycle/linked-list-cycle.cpp
@@ -18,10 +18,11 @@ public:
         
         while(fast->next!=NULL && fast->next->next!=NULL){
             fast=fast->next->next;
+             if(fast==slow)
+                return 1;
             slow=slow->next;
             
-            if(fast==slow)
-                return 1;
+           
         }
         return 0;
         
