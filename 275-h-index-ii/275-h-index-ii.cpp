3f65719878f6cd480ffc0486e6e5abaa397e540b
Abhishek Chauhan <76480451+abhishekchauhan15@users.noreply.github.com>
2022-02-04 23:57:36 +0530

    Time: 35 ms (18.77%), Space: 18.6 MB (48.56%) - LeetHub

---
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int start=0;
+        int n=citations.size();
+        int end=n-1;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            
+             if(citations[mid]>=n-mid)
+                end=mid-1;
+            else
+                start=mid+1;
+                
+        }
+        return n-start;
+        
+        
+    }
+};
