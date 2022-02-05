ef4439bd1f933f98e11a9d9d9a1db9f8c5b0b0ab
Abhishek Chauhan <76480451+abhishekchauhan15@users.noreply.github.com>
2022-02-05 18:47:12 +0530

    Time: 15 ms (84.98%), Space: 18.7 MB (48.65%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.cpp
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -8,15 +8,16 @@ public:
         while(start<=end){
             int mid=start+(end-start)/2;
             
+            if(citations[mid]==n-mid)
+                return citations[mid];
             
-             if(citations[mid]>=n-mid)
-                end=mid-1;
-            else
-                start=mid+1;
+            else if(citations[mid]<n-mid)
+                 start=mid+1;
+            
+             else
+                 end=mid-1;
                 
         }
         return n-start;
-        
-        
     }
 };
